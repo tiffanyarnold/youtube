904fbbadb5e06b9f4b847218ac2d7eeab57b35f1
tiffanyarnold <tiffanyoarnold@gmail.com>
2026-02-20 02:16:47 +0000

    fix

--- supabase/functions/get-channel/index.ts
+++ supabase/functions/get-channel/index.ts
@@ -1,3 +1,5 @@
+// deno-lint-ignore-file no-explicit-any
+// @ts-nocheck
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const corsHeaders = {
@@ -14,7 +16,7 @@ Deno.serve(async (req) => {
   try {
     const supabase = createClient(
       Deno.env.get("SUPABASE_URL")!,
-      Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
+      Deno.env.get("SUPABASE_SERVICE_KEY")!
     );
 
     const url = new URL(req.url);
